84361d8e3181af97d850f4ef53de838b8b5f3847
tinhlv <dxvantinh@gmail.com>
2023-02-04 10:20:17 +0700

    update code teams

---
+++ src/containers/OurSolutionsSection/index.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import { Image } from 'react-bootstrap'
+import logo01 from '../../assets/images/solutions/pic_solution_01.png';
+import logo02 from '../../assets/images/solutions/pic_solution_02.png';
+import logo03 from '../../assets/images/solutions/pic_solution_03.png';
+
+//slick slider
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Responsive = () => {
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        draggable: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+    return (
+        <Slider className="solutionsList" {...settings}>
+            <div className="solutionsList__item">
+                <a href='' title=''>
+                    <Image className="img-baner-right" src={logo01} fluid alt="logo" />
+                </a>
+            </div>
+            <div className="solutionsList__item">
+                <a href='' title=''>
+                    <Image className="img-baner-right" src={logo02} fluid alt="logo" />
+                </a>
+            </div>
+            <div className="solutionsList__item">
+                <a href='' title=''>
+                    <Image className="img-baner-right" src={logo03} fluid alt="logo" />
+                </a>
+            </div>
+            <div className="solutionsList__item">
+                <a href='' title=''>
+                    <Image className="img-baner-right" src={logo01} fluid alt="logo" />
+                </a>
+            </div>
+            <div className="solutionsList__item">
+                <a href='' title=''>
+                    <Image className="img-baner-right" src={logo02} fluid alt="logo" />
+                </a>
+            </div>
+            <div className="solutionsList__item">
+                <a href='' title=''>
+                    <Image className="img-baner-right" src={logo03} fluid alt="logo" />
+                </a>
+            </div>
+        </Slider>
+    );
+};
+
+const OurSolutionsSection = () => (
+    <div className='solutions_section'>
+        <div className='container'>
+            <h2 className='heading_section'>Our Solutions</h2>
+            <Responsive />
+        </div>
+    </div>
+)
+export default OurSolutionsSection
---
+++ src/containers/OurTeamSection/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const OurTeamSection = () => (
+    <div className='team_section'>
+        <div className="container">
+            <div className="text-center teams-heading">
+                <div className="teams-heading__title">
+                    <p className="text-brand">Med<span>2</span>Lab</p>
+                    <h3>Our Team</h3>
+                </div>
+                <p className="teams-heading__desc">The Med2Lab team bring together a desire to increase patient saftey through improving healthcare workers learning experince</p>
+            </div>
+        </div>
+    </div>
+)
+export default OurTeamSection
