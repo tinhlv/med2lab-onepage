56b98c550d1de91a06b1f62d67570d97de2a7f4f
tinhlv <dxvantinh@gmail.com>
2023-02-04 11:22:41 +0700

    slice html for team

--- src/containers/OurTeamSection/index.js
+++ src/containers/OurTeamSection/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import imageTeam02 from '../../assets/images/team/pic_team_02.png'
 
 const OurTeamSection = () => (
     <div className='team_section'>
@@ -8,7 +9,26 @@ const OurTeamSection = () => (
                     <p className="text-brand">Med<span>2</span>Lab</p>
                     <h3>Our Team</h3>
                 </div>
-                <p className="teams-heading__desc">The Med2Lab team bring together a desire to increase patient saftey through improving healthcare workers learning experince</p>
+                <p className="teams-heading__desc">The Med2Lab team bring together a desire to increase patient saftey through improving healthcare workers learning experince</p>
+            </div>
+            <div className="teams-detail-section">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="team-member">
+                            <div 
+                                className="team-member__avatar"
+                                style={{
+                                    backgroundImage: `url(${imageTeam02})`
+                                }}
+                            ></div>
+                            <div className="team-member__role">
+                                <span className="member__role--name">Dr. Bao Truong</span><br/>
+                                <span className="member__role--content">Co-Founder & CEO</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6"></div>
+                </div>
             </div>
         </div>
     </div>
