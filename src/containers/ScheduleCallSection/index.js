a3c893dfd28f663c4d9bd6b4ef4dd3d95209c432
tinhlv <dxvantinh@gmail.com>
2023-02-07 15:17:10 +0700

    update content 20230207

--- src/containers/ScheduleCallSection/index.js
+++ src/containers/ScheduleCallSection/index.js
@@ -5,7 +5,7 @@ const ScheduleCallSection = () => {
         <div className="schedue-call-section">
             <div className="container">
                 <div className="schedue-call-section__content">
-                    Speak to our team to see how <br/> we can change your healthworkers learning experience today
+                Contact our team to see how <br/> we can change your healthcare workers’ learning experience today
                 </div>
                 <a href="#!" className="btn btn_primary d-inline-flex mt-5">Schedule a Call <span className="icon-arrow-right ml-2"></span></a>
             </div>
